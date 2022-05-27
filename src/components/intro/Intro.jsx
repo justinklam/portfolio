3d91c2cc6de00ac9b55c2c87cda1fb1f1725441b
Justin Lam <justin.kh.lam@gmail.com>
2022-05-27 15:36:25 -0400

    add in elements to display name/titles

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,7 +4,21 @@ import "./intro.css";
 const Intro = () => {
   return (
     <div className="i">
-      <div className="i-left">left</div>
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">Hello, my name is</h2>
+          <h2 className="i-name">Justin</h2>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              <div className="i-title-item">Full-stack Developer</div>
+              <div className="i-title-item">Web Developer</div>
+              <div className="i-title-item">JavaScript Developer</div>
+              <div className="i-title-item">React Developer</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="i-right">right</div>
     </div>
   )
