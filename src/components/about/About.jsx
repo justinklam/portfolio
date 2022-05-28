32ceb36d361102bb0b22ea172009329b03b5c47e
Justin Lam <justin.kh.lam@gmail.com>
2022-05-28 14:26:02 -0400

    add in about section elements. add in image with placeholder art asset

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,6 +5,11 @@ const About = () => {
   return (
     <div classname="a">
       <div className="a-left"></div>
+        <div className="a-card bg"></div>
+        <div className="a-card">
+          <img src="https://images.pexels.com/photos/1194713/pexels-photo-1194713.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="a-img" />
+        </div>
+
       <div className="a-right"></div>
     </div>
   )
