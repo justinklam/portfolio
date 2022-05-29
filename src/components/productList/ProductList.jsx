907b80d26540cdc44aa85c0456ebe9a33649c200
Justin Lam <justin.kh.lam@gmail.com>
2022-05-28 20:38:05 -0400

    add in product list elements. add in rendering for Product component

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import "./productList.css";
+import Product from '../product/Product';
 
 const ProductList = () => {
   return (
     <div className="pl">
-      Product List
-
+      <div className="pl-texts">
+        <h1 className="pl-title">Placeholder</h1>
+        <p className="pl-desc">
+          Placeholder placeholder
+        </p>
+      </div>
+      <div className="pl-list">
+        <Product/>
+        <Product/>
+        <Product/>
+        <Product/>
+        <Product/>
+      </div>
     </div>
   )
 };
