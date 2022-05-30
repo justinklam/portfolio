ca4f911068fee64a709590a657f312131d29271a
Justin Lam <justin.kh.lam@gmail.com>
2022-05-30 15:52:25 -0400

    add in contact component into app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import About from "./components/about/About";
 import Intro from "./components/intro/Intro";
 import ProductList from "./components/productList/ProductList";
+import Contact from "./components/contact/Contact";
 
 const App = () => {
   return <div>
     <Intro/>
     <About/>
     <ProductList/>
+    <Contact/>
     </div>;
 };
 
