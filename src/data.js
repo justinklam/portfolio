177bafdb2beef19feefaa9593d1c74057835bf10
Justin Lam <justin.kh.lam@gmail.com>
2022-05-29 22:21:46 -0400

    add in data for product card rendering

---
+++ src/data.js
@@ -0,0 +1,38 @@
+import pomopetsImage from "./img/pomo-pets.png";
+import recipeRepoImage from "./img/recipe-repo.png";
+import jungleImage from "./img/jungle.png";
+import tweeterImage from "./img/tweeter.png";
+import weatherReactImage from "./img/weather-react.png";
+
+export const products = [
+  {
+    id: 1,
+    img: pomopetsImage,
+    link: "https://github.com/justinklam/pomopets"
+  },
+  {
+    id: 2,
+    img: recipeRepoImage,
+    link: "https://github.com/justinklam/recipe-repo"
+  },
+  {
+    id: 3,
+    img: jungleImage,
+    link: "https://github.com/justinklam/jungle"
+  },
+  {
+    id: 4,
+    img: scheduler,
+    link: "https://github.com/justinklam/scheduler"
+  },
+  {
+    id: 5,
+    img: tweeterImage,
+    link: "https://github.com/justinklam/tweeter"
+  },
+  {
+    id: 6,
+    img: weatherReactImage,
+    link: "https://github.com/justinklam/weather-react"
+  },
+]
