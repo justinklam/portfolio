0ee71160036fad9d56685e95a74af4836057e7b1
Justin Lam <justin.kh.lam@gmail.com>
2022-05-31 13:29:50 -0400

    add in icon elements/section for Email/Address

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./contact.css";
-import EmailImg from "../../img/email.png";
 import AddressImg from "../../img/address.png";
+import EmailImg from "../../img/email.png";
 import PhoneImg from "../../img/phone.png";
 
 const Contact = () => {
@@ -9,15 +9,25 @@ const Contact = () => {
     <div className="c">
       <div className="c-bg"></div>
       <div className="c-wrapper">
+
         <div className="c-left">
           <h1 className="c-title">Contact Me</h1>
           <div className="c-info">
             <div className="c-info-item">
               <img 
-                src="" 
+                src={EmailImg} 
                 alt="" 
                 className="c-icon" 
               />
+              E-mail me here!
+            </div>
+            <div className="c-info-item">
+              <img 
+                src={AddressImg} 
+                alt="" 
+                className="c-icon" 
+              />
+              Toronto, Ontario
             </div>
           </div>
         </div>
