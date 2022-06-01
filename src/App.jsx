44c1557d9c28dff5a73d0f0db5c4ae16a55299c4
Justin Lam <justin.kh.lam@gmail.com>
2022-06-01 14:17:56 -0400

    add in useContext and darkmode. add in conditional backgroundColor and text color change depending on darkMode state

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
+import { useContext } from "react";
+import { ThemeContext } from "./context";
+
+// Components
+import Toggle from "./components/toggle/Toggle";
 import About from "./components/about/About";
 import Intro from "./components/intro/Intro";
 import ProductList from "./components/productList/ProductList";
 import Contact from "./components/contact/Contact";
-import Toggle from "./components/toggle/Toggle";
 
 const App = () => {
 
-  return <div>
-    <Toggle/>
-    <Intro/>
-    <About/>
-    <ProductList/>
-    <Contact/>
-    </div>;
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+
+  return (
+    // if dark mode, make background color #222. else make white
+    <div
+      style={{
+        backgroundColor: darkMode ? "#222" : "white",
+        color: darkMode && "white",
+      }}
+    >
+      <Toggle/>
+      <Intro/>
+      <About/>
+      <ProductList/>
+      <Contact/>
+    </div>
+  );
 };
 
 export default App;
