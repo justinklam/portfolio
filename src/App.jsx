667259355d5703a7665e3eaf8a0f4249e720f2eb
Justin Lam <justin.kh.lam@gmail.com>
2022-06-01 13:55:14 -0400

    shift ThemeProvider into index.js to use in entire application

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,15 @@ import Intro from "./components/intro/Intro";
 import ProductList from "./components/productList/ProductList";
 import Contact from "./components/contact/Contact";
 import Toggle from "./components/toggle/Toggle";
-import { ThemeProvider } from "./context";
 
 const App = () => {
 
   return <div>
-    <ThemeProvider>
-      <Toggle/>
-      <Intro/>
-      <About/>
-      <ProductList/>
-      <Contact/>
-    </ThemeProvider>
+    <Toggle/>
+    <Intro/>
+    <About/>
+    <ProductList/>
+    <Contact/>
     </div>;
 };
 
