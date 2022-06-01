25ba491b3b957847ef869f633f3504f8e6dbb00c
Justin Lam <justin.kh.lam@gmail.com>
2022-05-31 21:55:09 -0400

    add in dark/light mode image assets. import image assets into the Toggle.jsx component

--- src/components/toggle/Toggle.jsx
+++ src/components/toggle/Toggle.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import "./toggle.css";
 
+// Art assets
+import Light from "../../img/light-mode.png";
+import Dark from "../../img/dark-mode.png";
+
 const Toggle = () => {
   return (
-    <div>Toggle</div>
+    <div className="t">
+      <img src="" alt="" className="t-icon"/>
+    </div>
   )
 };
 
