b60bc7e645e5a93f55a69a2900976c8dfc83561d
Justin Lam <justin.kh.lam@gmail.com>
2022-06-01 13:53:30 -0400

    add in ThemeProvider into App.js. place all other component as child

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import Intro from "./components/intro/Intro";
 import ProductList from "./components/productList/ProductList";
 import Contact from "./components/contact/Contact";
 import Toggle from "./components/toggle/Toggle";
+import { ThemeProvider } from "./context";
 
 const App = () => {
 
   return <div>
-    <Toggle/>
-    <Intro/>
-    <About/>
-    <ProductList/>
-    <Contact/>
+    <ThemeProvider>
+      <Toggle/>
+      <Intro/>
+      <About/>
+      <ProductList/>
+      <Contact/>
+    </ThemeProvider>
     </div>;
 };
 
