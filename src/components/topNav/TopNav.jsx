e2400d20e98d2fd38e38dde9a9d8c2851a035d4b
Justin Lam <justin.kh.lam@gmail.com>
2022-06-05 12:48:19 -0400

    add in elements for hamburger style menu

--- src/components/topNav/TopNav.jsx
+++ src/components/topNav/TopNav.jsx
@@ -9,9 +9,16 @@ const TopNav = () => {
         <div className="topbar-left">
           <a className="logo" href="/"><img src={Logo} alt="Logo" /></a>
         </div>
+
         <div className="topbar-right">
+          <div className="hamburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
         </div>
       </div>
+
     </div>
   )
 }
