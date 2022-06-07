d975fff38d4aa4613d8c8a90ff51768ac191f0e2
Justin Lam <justin.kh.lam@gmail.com>
2022-06-06 21:48:25 -0400

    comment out description

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -24,7 +24,7 @@ const Intro = () => {
             </div>
           </div>
           <p className="i-desc">
-            Full-stack developer and JavaScript enthusiast
+            {/* Full-stack developer and JavaScript enthusiast */}
           </p>
         </div>
       </div>
