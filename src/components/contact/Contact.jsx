d824d7241d6396faf44a86b944972dc06c228115
Justin Lam <justin.kh.lam@gmail.com>
2022-08-29 11:51:27 -0400

    update with a new-item for my resume

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,14 +1,14 @@
-import React, { useContext, useRef, useState } from 'react';
-import { ThemeContext } from '../../context';
-import emailjs from '@emailjs/browser';
+import React, { useContext, useRef, useState } from "react";
+import { ThemeContext } from "../../context";
+import emailjs from "@emailjs/browser";
 
 // CSS
 import "./contact.css";
 
 // Image Assets
-import LinkedInImg from "../../img/linkedIn.png";
-import GitHubImg from "../../img/github.png";
 import AddressImg from "../../img/address.png";
+import GitHubImg from "../../img/github.png";
+import LinkedInImg from "../../img/linkedIn.png";
 
 const Contact = () => {
   const formRef = useRef();
@@ -20,68 +20,99 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm(
-      'service_18rokgu', 
-      'template_nqqtns9', 
-      formRef.current, 
-      'KYowkfYBt_8fTeWiP'
+    emailjs
+      .sendForm(
+        "service_18rokgu",
+        "template_nqqtns9",
+        formRef.current,
+        "KYowkfYBt_8fTeWiP"
       )
-    .then((result) => {
-        console.log(result.text);
-        setComplete(true);
-        // to reset contact form text
-        e.target.reset();
-    }, (error) => {
-        console.log(error.text);
-    });
+      .then(
+        (result) => {
+          console.log(result.text);
+          setComplete(true);
+          // to reset contact form text
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
   return (
     <div className="c">
       <div className="c-bg"></div>
       <div className="c-wrapper">
-
         <div className="c-left">
           <h1 className="c-title">Contact Me</h1>
           <div className="c-info">
-
             <div className="c-info-item">
-              <a href="https://github.com/justinklam" style={{color: darkMode && "white"}}><img 
-                src={GitHubImg} 
-                alt="" 
-                className="c-icon" 
-              />
-              My GitHub</a>
+              <a
+                href="https://github.com/justinklam"
+                style={{ color: darkMode && "white" }}
+              >
+                <img src={GitHubImg} alt="" className="c-icon" />
+                My GitHub
+              </a>
             </div>
             <div className="c-info-item">
-              <a href="https://www.linkedin.com/in/justinkhlam/" style={{color: darkMode && "white"}}><img 
-                src={LinkedInImg} 
-                alt="" 
-                className="c-icon" 
-              />
-              My LinkedIn Profile</a>
+              <a
+                href="https://www.linkedin.com/in/justinkhlam/"
+                style={{ color: darkMode && "white" }}
+              >
+                <img src={LinkedInImg} alt="" className="c-icon" />
+                My LinkedIn Profile
+              </a>
             </div>
             <div className="c-info-item">
-              <a href="https://www.google.com/maps/place/Toronto,+ON/@43.717899,-79.6582408,10z/data=!3m1!4b1!4m5!3m4!1s0x89d4cb90d7c63ba5:0x323555502ab4c477!8m2!3d43.653226!4d-79.3831843" style={{color: darkMode && "white"}}><img 
-                  src={AddressImg} 
-                  alt="" 
-                  className="c-icon" 
-                />
-                Toronto, Ontario</a>
-              </div>
-
+              <a
+                href="https://www.canva.com/design/DAFKt14MvsU/Dy1hPQ189Nr-1om7ZuvK1A/view?utm_content=DAFKt14MvsU&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+                style={{ color: darkMode && "white" }}
+              >
+                <img src={AddressImg} alt="" className="c-icon" />
+                My Resume
+              </a>
+            </div>
+            <div className="c-info-item">
+              <a
+                href="https://www.google.com/maps/place/Toronto,+ON/@43.717899,-79.6582408,10z/data=!3m1!4b1!4m5!3m4!1s0x89d4cb90d7c63ba5:0x323555502ab4c477!8m2!3d43.653226!4d-79.3831843"
+                style={{ color: darkMode && "white" }}
+              >
+                <img src={AddressImg} alt="" className="c-icon" />
+                Toronto, Ontario
+              </a>
+            </div>
           </div>
         </div>
 
         <div className="c-right">
-          <p className="c-desc">
-              {/* <b>Reach out to me here!</b> */}
-          </p>
+          <p className="c-desc">{/* <b>Reach out to me here!</b> */}</p>
           <form ref={formRef} onSubmit={handleSubmit}>
-            <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Name" name="user_name"/>
-            <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Subect" name="user_subject"/>
-            <input style={{backgroundColor: darkMode && "#333"}} type="email" placeholder="Email" name="user_email"/>
-            <textarea style={{backgroundColor: darkMode && "#333"}} rows="5" placeholder="Message" name="message"></textarea>
+            <input
+              style={{ backgroundColor: darkMode && "#333" }}
+              type="text"
+              placeholder="Name"
+              name="user_name"
+            />
+            <input
+              style={{ backgroundColor: darkMode && "#333" }}
+              type="text"
+              placeholder="Subect"
+              name="user_subject"
+            />
+            <input
+              style={{ backgroundColor: darkMode && "#333" }}
+              type="email"
+              placeholder="Email"
+              name="user_email"
+            />
+            <textarea
+              style={{ backgroundColor: darkMode && "#333" }}
+              rows="5"
+              placeholder="Message"
+              name="message"
+            ></textarea>
             <button>Submit</button>
             {/* Return message on Submit press*/}
             {complete && " Thank you for your message!"}
@@ -89,7 +120,7 @@ const Contact = () => {
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Contact;
