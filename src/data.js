a31610bd6f3bd8c703acf6bba6d5f857a6a8ac91
Justin Lam <justin.kh.lam@gmail.com>
2022-08-31 20:12:50 -0400

    add react map-poi project. add in screenshot. remove jungle-ruby project

--- src/data.js
+++ src/data.js
@@ -3,41 +3,42 @@
 // Image Assets
 import pomopetsImage from "./img/pomo-pets.png";
 import recipeRepoImage from "./img/recipe-repo.png";
-import jungleImage from "./img/jungle.png";
 import schedulerImage from "./img/scheduler.png";
 import tweeterImage from "./img/tweeter.png";
 import tinyAppImage from "./img/tiny-app.png";
+import mapPOIImage from "./img/map-poi.png";
+// import jungleImage from "./img/jungle.png";
 // import weatherReactImage from "./img/weather-react.png";
 
 export const products = [
   {
     id: 1,
     img: pomopetsImage,
-    link: "https://github.com/justinklam/pomopets"
+    link: "https://github.com/justinklam/pomopets",
   },
   {
     id: 2,
     img: recipeRepoImage,
-    link: "https://github.com/justinklam/recipe-repo"
+    link: "https://github.com/justinklam/recipe-repo",
   },
   {
     id: 3,
-    img: jungleImage,
-    link: "https://github.com/justinklam/jungle"
+    img: mapPOIImage,
+    link: "https://github.com/justinklam/map-poi",
   },
   {
     id: 4,
     img: schedulerImage,
-    link: "https://github.com/justinklam/scheduler"
+    link: "https://github.com/justinklam/scheduler",
   },
   {
     id: 5,
     img: tweeterImage,
-    link: "https://github.com/justinklam/tweeter"
+    link: "https://github.com/justinklam/tweeter",
   },
   {
     id: 6,
     img: tinyAppImage,
-    link: "https://github.com/justinklam/tinyapp"
+    link: "https://github.com/justinklam/tinyapp",
   },
-]
+];
